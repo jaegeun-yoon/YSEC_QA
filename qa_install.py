0719431e002c4285e42f3e211ddd9f8c1e411500
jaegeun.yoon <98148447+jaegeun-yoon@users.noreply.github.com>
2023-01-04 12:34:22 +0900

    [Modify] google drive download

--- qa_install.py
+++ qa_install.py
@@ -1,14 +1,33 @@
 import zipfile
+import gdown
+import shutil
+import os.path
 
+def setup(url, zip_file):
 
-# download
-url = 'https://drive.google.com/file/d/1lD_1Os0zLCqK-rxJk6bN9C1aa3MOxTpT/view?usp=share_link'
+    gdown.download(url, zip_file, quiet=False)
 
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall('.')
 
+    # delete file
+    folder = '__MACOSX'
 
-# unpacking
-path_to_zip_file = '/Users/' + os.getenv('USER') + '/Downloads/Rooting_apk.zip' # 압축파일 경로
-directory_to_extract_to = '/Users/' + os.getenv('USER') + '/dev/Rooting_apk' # 압축 풀고 저장할 경로
+    try:
+        if os.path.isdir(folder) :
+            shutil.rmtree(folder)
 
-with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-    zip_ref.extractall(directory_to_extract_to)
+        if os.path.isfile(zip_file) :
+            os.remove(zip_file)
+
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
+
+if __name__ == "__main__":
+    # total 5m 5sec [28sec(133Mb) + 2min 38sec(658Mb) + 1min 59sec(466Mb)]
+    app = {"Rooting_apk.zip":"1lD_1Os0zLCqK-rxJk6bN9C1aa3MOxTpT", "RemoteControler_apk.zip":"1AR1d8QVmm1N5_PJbyrX57n09snzzdDhd", "PalleralCloner_apk.zip":"1xw-B_5MJvgOJzEsmSsZZP4WuaSB8-gGw"}
+    base_url = 'https://drive.google.com/uc?id='
+
+    for zip_file, id in app.items() :
+        url = base_url + id
+        setup(url, zip_file)
